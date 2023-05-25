f764478506c4554acc78889711a445bbbbea5f48
JamesOmare <jamesomare922@gmail.com>
2023-05-25 12:26:18 +0300

    added additional pages and signin basic functonality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+/* eslint-disable react/jsx-pascal-case */
 import React from 'react';
 import { Link, Route, Routes, useNavigate, useSearchParams } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components';
+import Header from './components/header-component/header-component';
+import Sign_In_Up from './pages/sign-in-up/sign-in-up.components';
 
 const HatsPage = () => (
   <div>
@@ -13,9 +17,12 @@ function App() {
   return (
     <div>
       {/* Switch -> Routes */}
+      <Header/>
        <Routes>
         <Route  path='/' element={<HomePage/>}/>
         <Route path='/hats' element={<HatsPage/>} />
+        <Route path='/shop' element={<ShopPage/>} />
+        <Route path='/signin' element={<Sign_In_Up/>} />
         </Routes>
     </div>
   );
---
+++ src/pages/sign-in-up/sign-in-up.components.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component.jsx";
+
+import "./sign-in-up.styles.scss";
+
+const Sign_In_Up = () => (
+    <div className="sign-in-up">
+        <SignIn />
+    </div>
+);
+
+export default Sign_In_Up;
